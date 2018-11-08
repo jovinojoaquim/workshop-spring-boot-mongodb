603a48f1f43532a6f57e7931b6a1fb66d03196f4
Jovino Joaquim <jovinojoaquim@hotmail.com>
2018-11-08 20:50:14 -0200

    Referenciaod os posts do usuário

--- src/main/java/com/workshopmongo/config/Instantiation.java
+++ src/main/java/com/workshopmongo/config/Instantiation.java
@@ -37,6 +37,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post("null", sdf.parse("07/11/2-18"), "Bom dia", "tenha um ótimo dia", new AuthorDTO(pedro));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
--- src/main/java/com/workshopmongo/domain/User.java
+++ src/main/java/com/workshopmongo/domain/User.java
@@ -1,44 +1,64 @@
 package com.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-public class User implements Serializable{
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
-	
-	public User() {}
-	
+
+	@DBRef
+	private List<Post> posts = new ArrayList<>();
+
+	public User() {
+	}
+
 	public User(String id, String name, String email) {
 		this.id = id;
-		this.name= name;
+		this.name = name;
 		this.email = email;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
@@ -65,6 +85,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 }
