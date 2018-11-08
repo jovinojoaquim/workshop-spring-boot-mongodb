1e878ec50b9d35b206e5a7b12e0b33705839ee7c
Jovino Joaquim <jovinojoaquim@hotmail.com>
2018-11-08 20:33:22 -0200

    Projeção dos dados do autor com DTO

--- src/main/java/com/workshopmongo/config/Instantiation.java
+++ src/main/java/com/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.workshopmongo.domain.Post;
 import com.workshopmongo.domain.User;
+import com.workshopmongo.dto.AuthorDTO;
+import com.workshopmongo.repository.PostRepository;
 import com.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,15 +19,24 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		User maria = new User(null, "Maria", "maria@email");
 		User alex = new User(null, "Alex", "alex@gmail.com");
 		User pedro = new User(null, "Pedro", "pedro@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, pedro));
+		
+		Post post1 = new Post("null", sdf.parse("07/11/2-18"), "Partiu viagem", "Vou viajar para São Paulo, abraços", new AuthorDTO(maria));
+		Post post2 = new Post("null", sdf.parse("07/11/2-18"), "Bom dia", "tenha um ótimo dia", new AuthorDTO(pedro));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/workshopmongo/domain/Post.java
@@ -0,0 +1,122 @@
+package com.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.workshopmongo.dto.AuthorDTO;
+
+@Document
+public class Post implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	private Date date;
+	private String body;
+	private String title;
+	private AuthorDTO author;
+
+	public Post(String id, Date date, String body, String title, AuthorDTO author) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.body = body;
+		this.title = title;
+		this.author = author;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((body == null) ? 0 : body.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (body == null) {
+			if (other.body != null)
+				return false;
+		} else if (!body.equals(other.body))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	
+}
